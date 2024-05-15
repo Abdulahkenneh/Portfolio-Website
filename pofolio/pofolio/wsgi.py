50b0e6a85398811fc12b2d3a857def3504776ae6
Abdulahkenneh <abulayeekennedy@gmail.com>
2024-05-15 22:38:26 +0200

    last confuse procfile

--- pofolio/pofolio/wsgi.py
+++ pofolio/pofolio/wsgi.py
@@ -6,10 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
-# from dj_static import Cling
+from dj_static import Cling
 import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pofolio.settings')
 
-# application = get_wsgi_application()
-# application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+application = Cling(get_wsgi_application())
