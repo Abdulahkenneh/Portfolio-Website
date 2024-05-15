ea76a37c41a4adafbf3e54b49c5ce10ca5579ea6
Abdulahkenneh <abulayeekennedy@gmail.com>
2024-05-15 09:20:14 +0200

    Ready for deployment to heroku

--- pofolio/pofolio/settings.py
+++ pofolio/pofolio/settings.py
@@ -48,6 +48,25 @@ INSTALLED_APPS = [
 
 ]
 
+# Heroku settings
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure().
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    # Allow all host headers.
+    ALLOWED_HOSTS = ['*']
+    # Static asset configuration
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
